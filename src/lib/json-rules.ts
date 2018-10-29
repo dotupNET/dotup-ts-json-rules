68adeaf14a1d6d2632e1e7cf68ee5d837396da1f
Peter Ullrich <p.ullrich@dotup.de>
2018-10-29 13:24:18 +0100

    source documented

--- src/lib/json-rules.ts
+++ src/lib/json-rules.ts
@@ -1,10 +1,26 @@
 import { RuleSet } from './rule-set';
 import { PropertyValidator } from './property-validator';
 
+/**
+ *  With JsonRules you can validate objects with given rules.
+ * 
+ * @export
+ * @class JsonRules
+ */
 export class JsonRules {
 
   constructor() { }
 
+  /**
+   *  IsValid validates the given rule set against the source object.
+   * 
+   * If there are selectors definded which does not  
+   *
+   * @param {*} source Object to test.
+   * @param {RuleSet} ruleSet The rules to validate.
+   * @returns {boolean} Ture if the source matches the rules, otherwise false.
+   * @memberof JsonRules
+   */
   IsValid(source: any, ruleSet: RuleSet): boolean {
     if (this.ShouldValidateRule(source, ruleSet)) {
       if (this.RuleMatches(source, ruleSet.rules)) {
@@ -17,15 +33,23 @@ export class JsonRules {
     }
   }
 
-  ShouldValidateRule(source: any, ruleSet: RuleSet) {
+  /**
+   *  Returns true if no selector is defined or all of the given selectors matches with the source object. Otherwise false.
+   * 
+   * @param {*} source Object to test.
+   * @param {RuleSet} ruleSet The rules to validate.
+   * @returns {boolean} True if the rule should be used, otherwise false.
+   * @memberof JsonRules
+   */
+  ShouldValidateRule(source: any, ruleSet: RuleSet): boolean {
     const ruleSetKey = 'selectors';
 
-    if (ruleSet.selectors === undefined || ruleSet.selectors === null){
+    if (ruleSet.selectors === undefined || ruleSet.selectors === null) {
       // No selector is set, apply all rules
       return true;
     }
 
-    const ruleSetSelectors : any = ruleSet.selectors;
+    const ruleSetSelectors: any = ruleSet.selectors;
 
     for (const ruleSetSelector in ruleSetSelectors) {
       if (!ruleSetSelectors.hasOwnProperty(ruleSetSelector)) {
@@ -42,7 +66,15 @@ export class JsonRules {
     return true;
   }
 
-  SelectorMatches(values: any, rule: any) {
+  /**
+   *  Checks if the selector matches.
+   * 
+   * @param {*} values Values to check.
+   * @param {*} rule Selector to validate
+   * @returns {boolean} True if the selector matches, otherwise false
+   * @memberof JsonRules
+   */
+  SelectorMatches(values: any, rule: any): boolean {
     const result = true;
     const validator = <any>new PropertyValidator();
     for (const key in rule) {
@@ -69,6 +101,14 @@ export class JsonRules {
     return result;
   }
 
+  /**
+   *  Checks if the rule matches.
+   *
+   * @param {*} values Values to check.
+   * @param {*} rule Rule to validate
+   * @returns {boolean} True if the rule matches, otherwise false
+   * @memberof JsonRules
+   */
   RuleMatches(values: any, rule: any): boolean {
     const result = true;
     const validator = <any>new PropertyValidator();
--- src/lib/rule-set.ts
+++ src/lib/rule-set.ts
@@ -1,4 +1,12 @@
-export class RuleSet {
+/**
+ *  This class is to define rules.
+ * 
+ * TODO: make it generic
+ *
+ * @export
+ * @class RuleSet
+ */
+ export class RuleSet {
   selectors?: {};
   rules: {};
 }
